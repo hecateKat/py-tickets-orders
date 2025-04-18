7091405cf30ae9fba21ac79ac938cb1267ae2824
Katarzyna Ochmanska <kat.ochmanska@gmail.com>
2025-04-18 11:11:27 +0200

    review

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -138,6 +138,9 @@ class OrderSerializer(serializers.ModelSerializer):
         with transaction.atomic():
             tickets_data = validated_data.pop("tickets")
             order = Order.objects.create(**validated_data)
-            for ticket_data in tickets_data:
-                Ticket.objects.create(order=order, **ticket_data)
+            tickets = [
+                Ticket(order=order, **ticket_data)
+                for ticket_data in tickets_data
+            ]
+            Ticket.objects.bulk_create(tickets)
             return order
